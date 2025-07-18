0acd08f80fa285d687efb939d6ea0e24a8247b91
jlee-armorock <jlee@armorock.com>
2025-07-18 17:37:16 -0500

    get rid of desktop constraint view

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import "./global.css";
 import { ReactNode } from "react";
-import DesktopViewport from "../components/DesktopViewport";
-import ViewportDebug from "../components/ViewportDebug";
 
 export const metadata = {
   title: `Website`,
@@ -15,15 +13,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* This viewport meta will be dynamically updated by DesktopViewport component */}
-        <meta name="viewport" content="width=1440, initial-scale=1.0, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-open-sans">
-        <DesktopViewport />
         {children}
-        <ViewportDebug />
       </body>
     </html>
   );
