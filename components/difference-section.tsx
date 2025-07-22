ad9595225cd45393f5b9dfd97e550590b5201ed8
jlee-armorock <jlee@armorock.com>
2025-07-22 13:52:08 -0500

    refactor: update text styles for improved readability in difference section

--- components/difference-section.tsx
+++ components/difference-section.tsx
@@ -10,7 +10,7 @@ const DifferenceSection: NextPage<DifferenceSectionType> = ({
 }) => {
   return (
     <section
-      className={`self-stretch overflow-hidden shrink-0 flex flex-col items-center justify-start py-16 px-[120px] box-border gap-[38px] bg-[url('/difference-section@3x.png')] bg-cover bg-no-repeat bg-[top] max-w-full text-center text-6xl font-oswald md:gap-[19px] md:py-10 md:px-[30px] sm:py-8 sm:px-4 sm:gap-[12px] xs:py-6 xs:px-3 xs:gap-[8px] ${className}`}
+      className={`self-stretch overflow-hidden shrink-0 flex flex-col items-center justify-start py-16 px-[120px] box-border gap-[38px] bg-[url('/difference-section@3x.png')] bg-cover bg-no-repeat bg-[top] max-w-full text-center text-6xl font-oswald md:gap-[19px] md:py-10 md:px-[30px] sm:py-8 sm:px-4 xs:py-6 xs:px-3 ${className}`}
     >
       <div className="w-[663px] h-[213px] flex flex-col items-start justify-start max-w-full">
         <div className="w-[663px] flex flex-row items-start justify-center py-0 px-5 box-border max-w-full">
@@ -41,7 +41,7 @@ const DifferenceSection: NextPage<DifferenceSectionType> = ({
               strength
             </h2>
           </div>
-          <div className="w-[342px] h-[175px] relative text-xl leading-[35px] font-open-sans text-black whitespace-pre-wrap flex items-center justify-center z-[1] md:w-full md:h-auto md:text-xl md:leading-relaxed sm:text-xl xs:text-lg">{`Our polymer concrete structures  have a compression strength of 11,000 psi+ and flexural strength of 1,800 psi+, a stronger and more yield-resistant product compared to traditional precast concrete. `}</div>
+          <div className="w-[342px] h-[175px] relative text-xl leading-[35px] font-open-sans text-black whitespace-pre-wrap flex items-center justify-center z-[1] md:w-full md:h-auto md:text-xl md:leading-relaxed sm:text-xl xs:text-sm">{`Our polymer concrete structures  have a compression strength of 11,000 psi+ and flexural strength of 1,800 psi+, a stronger and more yield-resistant product compared to traditional precast concrete. `}</div>
         </section>
         <section className="h-[300px] w-[390px] shadow-[25px_25px_4px_rgba(0,_0,_0,_0.25)] bg-white flex flex-col items-start justify-start pt-[25px] pb-[51px] pl-[35px] pr-[29px] box-border gap-3.5 text-center text-3xl text-gray-100 font-oswald md:w-[48%] md:h-[280px] sm:h-[260px] xs:h-[240px] md:pt-4 md:px-4 md:pb-4 md:gap-4 sm:gap-3 xs:gap-2">
           <div className="w-[390px] relative shadow-[25px_25px_4px_rgba(0,_0,_0,_0.25)] bg-white h-[300px] hidden" />
@@ -50,7 +50,7 @@ const DifferenceSection: NextPage<DifferenceSectionType> = ({
               sustainability
             </h2>
           </div>
-          <div className="w-[326px] relative text-xl leading-[35px] font-open-sans text-black flex items-center justify-center z-[1] md:w-full md:h-auto md:text-xl md:leading-relaxed sm:text-xl xs:text-lg">
+          <div className="w-[326px] relative text-xl leading-[35px] font-open-sans text-black flex items-center justify-center z-[1] md:w-full md:h-auto md:text-xl md:leading-relaxed sm:text-xl xs:text-sm">
             Using polymer concrete reduces the frequency of replacements, minimizing environmental footprint and offering a sustainable solution for wastewater systems.
           </div>
         </section>
@@ -66,7 +66,7 @@ const DifferenceSection: NextPage<DifferenceSectionType> = ({
               <span className="hidden sm:hidden md:hidden">50-year corrosion warranty</span>
             </h3>
           </div>
-          <div className="w-[365px] h-[157px] relative text-xl leading-[35px] font-open-sans text-black flex items-center justify-center z-[1] md:w-full md:h-auto md:text-xl md:leading-relaxed sm:text-xl xs:text-lg">
+          <div className="w-[365px] h-[157px] relative text-xl leading-[35px] font-open-sans text-black flex items-center justify-center z-[1] md:w-full md:h-auto md:text-xl md:leading-relaxed sm:text-xl xs:text-sm">
             Our long term reliability significantly reduces the need for costly rehabilitation and maintenance, providing unparalleled economic benefits.
           </div>
         </section>
@@ -77,7 +77,7 @@ const DifferenceSection: NextPage<DifferenceSectionType> = ({
               iso 9001-2015 certified
             </h2>
           </div>
-          <div className="w-[362px] h-[175px] relative text-xl leading-[35px] font-open-sans text-black flex items-center justify-center z-[1] md:w-full md:h-auto md:text-xl md:leading-relaxed sm:text-xl xs:text-lg">
+          <div className="w-[362px] h-[175px] relative text-xl leading-[35px] font-open-sans text-black flex items-center justify-center z-[1] md:w-full md:h-auto md:text-xl md:leading-relaxed sm:text-xl xs:text-sm">
             We are an ISO 9001 Certified polymer manufacturer. Our quality control systems and procedures are maintained under the highest standards found in the United States.
           </div>
         </section>
@@ -88,7 +88,7 @@ const DifferenceSection: NextPage<DifferenceSectionType> = ({
               lead times
             </h2>
           </div>
-          <div className="w-[362px] h-[175px] relative text-xl leading-[35px] font-open-sans text-black flex items-center justify-center z-[1] md:w-full md:h-auto md:text-xl md:leading-relaxed sm:text-xl xs:text-lg">{`We offer faster, more efficient installation when time is critical. For small structures, lead times are 2-4 weeks whereas large structures are 4-6 weeks. We can also expedite production based on availability. `}</div>
+          <div className="w-[362px] h-[175px] relative text-xl leading-[35px] font-open-sans text-black flex items-center justify-center z-[1] md:w-full md:h-auto md:text-xl md:leading-relaxed sm:text-xl xs:text-sm">{`We offer faster, more efficient installation when time is critical. For small structures, lead times are 2-4 weeks whereas large structures are 4-6 weeks. We can also expedite production based on availability. `}</div>
         </section>
         <section className="h-[300px] w-[390px] shadow-[25px_25px_4px_rgba(0,_0,_0,_0.25)] bg-white flex flex-col items-start justify-start pt-[26px] pb-[49px] pl-[9px] pr-0.5 box-border gap-[15px] text-center text-3xl text-gray-100 font-oswald md:w-[48%] md:h-[280px] sm:h-[260px] xs:h-[240px] md:pt-4 md:px-4 md:pb-4 md:gap-4 sm:gap-3 xs:gap-2">
           <div className="w-[390px] relative shadow-[25px_25px_4px_rgba(0,_0,_0,_0.25)] bg-white h-[300px] hidden" />
@@ -97,7 +97,7 @@ const DifferenceSection: NextPage<DifferenceSectionType> = ({
               monolithic pouring
             </h2>
           </div>
-          <div className="w-[379px] relative text-xl leading-[35px] font-open-sans text-black flex items-center justify-center z-[1] md:w-full md:h-auto md:text-xl md:leading-relaxed sm:text-xl xs:text-lg">
+          <div className="w-[379px] relative text-xl leading-[35px] font-open-sans text-black flex items-center justify-center z-[1] md:w-full md:h-auto md:text-xl md:leading-relaxed sm:text-xl xs:text-sm">
             We use a monolithic pouring system for all of our structures, including benching, to mitigate opportunities for Inflow and Infiltration and minimize the risk of leaks and cracks in structures over time.
           </div>
         </section>
