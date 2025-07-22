2b3510cd3a048f9b11cec12cec432c9451273e47
jlee-armorock <jlee@armorock.com>
2025-07-22 14:40:00 -0500

    Add new favicon images and site web manifest
    - Added favicon images: favicon-16x16.png, favicon-32x32.png, and favicon.ico.
    - Created site.webmanifest file with app icons and color settings.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,23 @@ import "./global.css";
 import { ReactNode } from "react";
 
 export const metadata = {
-  title: `Website`,
+  title: `Armorock Polymer Concrete`,
+  icons: {
+    icon: [
+      { url: '/favicon/favicon.ico' },
+      { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/favicon/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+    other: [
+      {
+        rel: 'manifest',
+        url: '/favicon/site.webmanifest',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
